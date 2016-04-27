77b889fc02ed1049a296b185d4f67de8b3a1a007
Tecnico - TI <tecnico@epfserver.com.br>
2016-04-26 22:37:58 -0300

    Criação dos metodos Somar, Subtrair, Dividir e Multiplicar

--- src/pkg_MeuPrimeiroProjetoVersionado/Classe1Versionada.java
+++ src/pkg_MeuPrimeiroProjetoVersionado/Classe1Versionada.java
@@ -2,6 +2,21 @@ package pkg_MeuPrimeiroProjetoVersionado;
 
 public class Classe1Versionada {
 
+	private int valorUm;
+	private int valorDois;
+	
+	public void somar(){
+		
+	}
+	public void subtrair(){
+		
+	}
+	public void dividir(){
+		
+	}
+	public void multiplicar(){
+		
+	}
 	public static void main(String[] args) {
 		System.out.print("Teste x");
 	}
