8471df6cd1ad8bee7d8e2547f0aa8dedd2135327
Tecnico - TI <tecnico@epfserver.com.br>
2016-04-26 22:30:04 -0300

    Salvando alteração da classe

--- src/pkg_MeuPrimeiroProjetoVersionado/Classe1Versionada.java
+++ src/pkg_MeuPrimeiroProjetoVersionado/Classe1Versionada.java
@@ -3,8 +3,6 @@ package pkg_MeuPrimeiroProjetoVersionado;
 public class Classe1Versionada {
 
 	public static void main(String[] args) {
-		System.out.print("Teste");
-
+		System.out.print("Teste x");
 	}
-
 }
