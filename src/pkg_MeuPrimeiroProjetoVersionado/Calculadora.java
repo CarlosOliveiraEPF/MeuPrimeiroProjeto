eb83b75c8648ffcc9683012e979bbba8964c0382
Tecnico - TI <tecnico@epfserver.com.br>
2016-05-02 22:38:46 -0300

    Aula FI de 02/05/2016, criado o método SOMAR()

---
+++ src/pkg_MeuPrimeiroProjetoVersionado/Calculadora.java
@@ -0,0 +1,30 @@
+package pkg_MeuPrimeiroProjetoVersionado;
+
+public class Calculadora {
+
+	//private int valorUm;
+	//private int valorDois;
+	private int valor;
+	
+	public void somar(int ValorUm,  int ValorDois){
+		//this.valorUm=ValorUm;
+		//this.valorDois=ValorDois;		
+		//int soma = this.valorUm+this.valorDois;
+		//return soma;
+		this.valor += (ValorUm+ValorDois);
+	}
+	public void subtrair(){
+		
+	}
+	public void dividir(){
+		
+	}
+	public void multiplicar(){
+		
+	}
+	public int getValor() {
+		return valor;
+	}
+	
+	
+}
---
+++ src/pkg_MeuPrimeiroProjetoVersionado/Principal.java
@@ -0,0 +1,18 @@
+package pkg_MeuPrimeiroProjetoVersionado;
+
+import javax.swing.JOptionPane;
+
+public class Principal {
+
+	public static void main(String[] args) {
+	
+		Calculadora calculadora = new Calculadora();
+		calculadora.somar(10,50);
+		calculadora.somar(20,10);
+		//JOptionPane.showMessageDialog(null, retornoCalculo);
+		JOptionPane.showMessageDialog(null, calculadora.getValor(), "A resultado da operação é...", 0);
+		JOptionPane.showMessageDialog(null, calculadora.getValor(), "A resultado da operação é...", 1);
+		JOptionPane.showMessageDialog(null, calculadora.getValor(), "A resultado da operação é...", 2);
+		JOptionPane.showMessageDialog(null, calculadora.getValor(), "A resultado da operação é...", 3);
+	}
+}
