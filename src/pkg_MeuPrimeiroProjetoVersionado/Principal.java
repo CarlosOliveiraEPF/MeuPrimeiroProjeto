028e1c11dd55d0646bcb9b69297ce667901bba23
Tecnico - TI <tecnico@epfserver.com.br>
2016-05-09 21:36:40 -0300

    Implementação do método Multiplicar.

--- src/pkg_MeuPrimeiroProjetoVersionado/Principal.java
+++ src/pkg_MeuPrimeiroProjetoVersionado/Principal.java
@@ -7,8 +7,11 @@ public class Principal {
 	public static void main(String[] args) {
 	
 		Calculadora calculadora = new Calculadora();
-		calculadora.somar(10,50);
-		calculadora.somar(20,10);
+//		calculadora.somar(10,50);
+//		calculadora.somar(20,10);
+		int primeiroValor = Integer.parseInt(JOptionPane.showInputDialog("Digite o Primeiro Valor: "));
+		int segundoValor = Integer.parseInt(JOptionPane.showInputDialog("Digite o Segundo Valor: "));
+		calculadora.multiplicar(primeiroValor,segundoValor);
 		//JOptionPane.showMessageDialog(null, retornoCalculo);
 		JOptionPane.showMessageDialog(null, calculadora.getValor(), "A resultado da operação é...", 0);
 		JOptionPane.showMessageDialog(null, calculadora.getValor(), "A resultado da operação é...", 1);
